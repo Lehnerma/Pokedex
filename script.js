0d4490304ee207d1cfef3df244d2d014bb24126b
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-03-04 22:49:10 +0100

    adding the landing page and try the styl of an opening pokeball - with help from ai -> mistake delete evrything and write it on my selfe.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //base url erstellen
 // url for offste
 // function for fetching
-// loading spinner integretting 
+// loading spinner integretting
+function init() {
+  initBtn();
+}
+
+function initBtn() {
+  const ballBtn = document.getElementById("ball_btn");
+  ballBtn.addEventListener("click", () => {
+    // erst die shake bewegeung dann das öffnen.
+    ballBtn.classList.add("shake");
+    setTimeout(() => {
+      ballBtn.classList.remove("shake");
+      document.body.classList.add("open");
+    }, 500);
+  });
+}
